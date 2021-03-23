cdd53bd91a768f2d073b77da56fd813e3aef64b0
Erik Mortimer <erikmortimermail@gmail.com>
2021-03-23 09:28:46 -0400

    Created Restaurant page and model, tested with in-memory data

---
+++ FoodApp.Data/IRestaurantData.cs
@@ -0,0 +1,37 @@
+﻿using FoodApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodApp.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Erik's Pizza", Location = "Boston", Cuisine=CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Casa Blanca", Location = "Madison", Cuisine=CuisineType.Mexican },
+                new Restaurant { Id = 3, Name = "Indian Food Restaurant", Location = "Washington", Cuisine = CuisineType.Indian }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
--- FoodApp/Pages/Restaurants/List.cshtml.cs
+++ FoodApp/Pages/Restaurants/List.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodApp.Core;
+using FoodApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -11,17 +13,27 @@ namespace FoodApp.Pages.Restaurants
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
 
+        // These two statements below would be considered "output model"
+        // Used to populate information
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
-        public ListModel(IConfiguration config)
+        [BindProperty(SupportsGet=true)] // This uses SearchTerm as a input and output model
+        public string SearchTerm { get; set; }
+
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
+            this.restaurantData = restaurantData;
         }
 
         public void OnGet()
         {
             Message = config["Message"];
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
 }
