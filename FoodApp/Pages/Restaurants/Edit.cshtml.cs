cddb33028ef8eb7c5ade5d8f57211bd43a35806d
Erik Mortimer <erikmortimermail@gmail.com>
2021-03-25 07:21:17 -0400

    Created edit form layout; added libman.json for lib deps

--- FoodApp/Pages/Restaurants/Edit.cshtml.cs
+++ FoodApp/Pages/Restaurants/Edit.cshtml.cs
@@ -6,22 +6,27 @@ using FoodApp.Core;
 using FoodApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FoodApp.Pages.Restaurants
 {
     public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
 
         public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
 
-        public EditModel(IRestaurantData restaurantData)
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
         }
 
         public IActionResult OnGet(int restaurantId)
         {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
             Restaurant = restaurantData.GetById(restaurantId);
             if(Restaurant == null)
             {
